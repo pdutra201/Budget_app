863866daf39b3e00ad501ec7754cc73a3f693ece
philip dutra <monsterphilip10@gmail.com>
2023-12-31 23:05:38 -0500

    migrated users table

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from config import app
 from flask_restful import Resource
-
+from config import app, db, api
+from models import User
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -3,7 +3,7 @@ from sqlalchemy_serializer import SerializerMixin
 
 from config import db, bcrypt
 
-class Users(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -11,19 +11,18 @@ class Users(db.Model):
     _password_hash = db.Column(db.String)
 
     @hybrid_property
-    def password_hash( self):
+    def password_hash(self):
         raise Exception('Password hashes may not be viewed.')
-    
+
     @password_hash.setter
     def password_hash(self, password):
         password_hash = bcrypt.generate_password_hash(
-            password.encode('uyf-8'))
+            password.encode('utf-8'))
         self._password_hash = password_hash.decode('utf-8')
 
     def authenticate(self, password):
         return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8')
-        )
-    
+            self._password_hash, password.encode('utf-8'))
+
     def __repr__(self):
-        return f'User {self.username, ID: {self.id}}'
+        return f'User {self.username}, ID: {self.id}'
