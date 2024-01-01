d4912c00e8ba320d433130680b7fda1ee4c2290a
philip dutra <monsterphilip10@gmail.com>
2023-12-31 21:14:45 -0500

    initalized user model

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from config import app
+from flask_restful import Resource
+
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
---
+++ server/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+
+from config import db, bcrypt
+
+class Users(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+
+    @hybrid_property
+    def password_hash( self):
+        raise Exception('Password hashes may not be viewed.')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('uyf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+    
+    def __repr__(self):
+        return f'User {self.username, ID: {self.id}}'
