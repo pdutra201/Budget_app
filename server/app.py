8fa44c973009098c51289521cbf03bfad54769a0
philip dutra <monsterphilip10@gmail.com>
2024-03-13 00:23:22 -0400

    completed edit and newTransaction functions

--- server/app.py
+++ server/app.py
@@ -76,7 +76,6 @@ class Transactions(Resource):
         user_id = session.get('user_id')
         if(user_id):
             transaction = Transaction.query.filter(Transaction.id == trans_id, Transaction.user_id == user_id ).first()
-            print(transaction)
             if transaction:
                 db.session.delete(transaction)
                 db.session.commit()
@@ -85,6 +84,45 @@ class Transactions(Resource):
                 return {'error': 'Transaction not found'}, 404
         else:
             return {'error': 'not authorized'}, 401
+        
+
+    def post(self):
+        data = request.get_json()
+        
+        dateObj = datetime.strptime(data['date'], "%Y-%m-%dT%H:%M:%S.%fZ")
+        
+        print('hi')
+        try:
+            newTrans = Transaction(
+                amount = float(data['amount']),
+                description = data['description'],
+                date = dateObj,
+                user_id = data['user_id']
+                
+            )
+            db.session.add(newTrans)
+            db.session.commit()
+            return newTrans.to_dict(), 201
+        except ImportError:
+            return {}
+        
+    def put(self, trans_id):
+        user_id = session['user_id']
+        data = request.get_json()
+        transaction = Transaction.query.filter(Transaction.id == trans_id, Transaction.user_id == user_id ).first()
+        if(transaction):
+            transaction.amount = data['amount']
+            transaction.description = data['description']
+            transaction.date = datetime.strptime(data['date'], "%Y-%m-%dT%H:%M:%S.%fZ")
+            
+            db.session.commit()
+            
+            return {'message': 'Transaction updated successfully'}, 200
+        
+        else:
+            return {'error': 'Edit could not be completed'}, 400
+
+
 
 
 
